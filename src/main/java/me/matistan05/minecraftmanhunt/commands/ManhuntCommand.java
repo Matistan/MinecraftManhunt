80a90c553542da73b4cbb15038be6e6020c77f85
Matistan <matistan05@gmail.com>
2023-04-16 22:12:59 +0200

    showing if the player id dead

--- src/main/java/me/matistan05/minecraftmanhunt/commands/ManhuntCommand.java
+++ src/main/java/me/matistan05/minecraftmanhunt/commands/ManhuntCommand.java
@@ -364,6 +364,8 @@ public class ManhuntCommand implements CommandExecutor {
                             } else {
                                 if(!target.getWorld().getEnvironment().equals(hunter.getWorld().getEnvironment())) {
                                     meta.setDisplayName(ChatColor.RED + target.getName() + " is not in this dimension!");
+                                } else if(target.isDead()){
+                                    meta.setDisplayName(ChatColor.RED + target.getName() + "is dead!");
                                 } else {
                                     meta.setDisplayName(ChatColor.GOLD + "Tracking: " + ChatColor.GREEN + whichSpeedrunner.get(i));
                                 }
