10a4f149ca1b75b90482cdff830d5544a36d87c7
Matistan <matistan05@gmail.com>
2023-04-18 19:31:17 +0200

    created DamageListener

---
+++ src/main/java/me/matistan05/minecraftmanhunt/listeners/DamageListener.java
@@ -0,0 +1,30 @@
+package me.matistan05.minecraftmanhunt.listeners;
+
+import me.matistan05.minecraftmanhunt.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import static me.matistan05.minecraftmanhunt.commands.ManhuntCommand.*;
+
+public class DamageListener implements Listener {
+    private final Main main;
+    public DamageListener(Main main) {
+        this.main = main;
+        Bukkit.getPluginManager().registerEvents(this, main);
+    }
+    @EventHandler
+    public void DamageEvent(EntityDamageEvent e) {
+        if(!(e.getEntity() instanceof Player)) {
+            return;
+        }
+        Player player = (Player) e.getEntity();
+        if(inGame && ((pausePlayers.size() == (hunters.size() + speedrunners.size()) && (hunters.contains(player.getName()) || speedrunners.contains(player.getName()))) ||
+                (seconds != main.getConfig().getInt("headStartDuration") && hunters.contains(player.getName())))) {
+            e.setCancelled(true);
+        }
+    }
+
+}
